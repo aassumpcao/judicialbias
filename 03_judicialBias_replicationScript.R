0ef8702a7bf41b5e2d5f8b9dfc3b6525993ce71d
aassumpcao <andre.assumpcao@gmail.com>
2018-12-17 09:01:56 -0500

    pushing local files

---
+++ 03_judicialBias_replicationScript.R
@@ -0,0 +1,14 @@
+### judicial favoritism of politicians: analysis script
+# to be updated
+# by andre.assumpcao
+
+# import statements
+library(here)
+library(tidyverse)
+library(magrittr)
+library(readxl)
+library(AER)
+library(stargazer)
+library(lfe)
+library(extrafont)
+
