75b31e28fb6bb6a9b1ffbeeaaa24e67a95731c4e
aassumpcao <andre.assumpcao@gmail.com>
2019-02-01 17:23:26 -0500

    checked and tested first function

--- draft_tjsp.py
+++ draft_tjsp.py
@@ -1,3 +1,4 @@
+# python
 # import statements
 import codecs
 import glob
@@ -30,96 +31,46 @@ WINDOW_SIZE      ='1920,1080'
 
 # set options
 chrome_options = Options()
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--window-size=%s' % WINDOW_SIZE)
 chrome_options.binary_location = CHROME_PATH
-
 # open invisible browser
 browser = webdriver.Chrome(executable_path = CHROMEDRIVER_PATH)
-
-# # open invisible browser
-# browser = webdriver.Chrome(executable_path = CHROMEDRIVER_PATH,
-#                            chrome_options  = chrome_options)
-
 # set implicit wait for page load
 browser.implicitly_wait(60)
-
 from tjsp import *
 
-scraper(browser).sct_case('Paulo Maluf')
+scraper(browser).sct_case('Fernando Haddad')
+scraper(browser).sct_case('Nathan Jensen')
 
 ### methods code
-################################# every url entry should become self.url
-################################# every browser entry should become self.browser
-url      = 'https://esaj.tjsp.jus.br/cjpg/'
-classSCT = 'Procedimento do Juizado Especial Cível'
-
-
-# store politician's name
-name = 'Fernando Haddad'
-
-# store politician's name in class object
-name = 'Paulo Maluf'
-
-# navigate to page
-browser.get(url)
-
-# search parameters
-# find text box to write politicians' name in and case class
-nameid  = 'iddadosConsulta.pesquisaLivre'
-classid = 'classe_selectionText'
-
-# click to search 
-sbtpath = '//*[(@id = "pbSubmit")]'
-
-# find total number of cases in page
 numbers = '//*[contains(concat( " ", @class, " " ),' + \
-    ' concat( " ", "fonteNegrito", " " ))]'
+            ' concat( " ", "fonteNegrito", " " ))]'
 
-# fint total number of cases in all pages and extract it
-results = '//*[@class = "espacamentoCimaBaixo"]' + \
-    '//*[contains(text(), "Resultados")]'
-regex0  = re.compile('(?<=de )[0-9]+', re.IGNORECASE)
-regex1  = re.compile('[0-9]+(?= a)', re.IGNORECASE)
+error = '//*[(@class = "aviso espacamentoCimaBaixo' + \
+            ' centralizado fonteNegrito")]'
 
-# find next page button
-nextpage = '//*[@title = "Próxima página"]'
+search = '//*[contains(@class, "espacamentoCimaBaixo")]'
 
-# find 'pesquisa livre' text box and send politician's name
-browser.find_element_by_id(nameid).send_keys(name)
+searched = browser.find_element_by_xpath(search).text
 
-# find 'classe' text box and send special civil tribunals id
-WebDriverWait(browser, .5)
-sctclass = browser.find_element_by_id(classid)
-sctclass.send_keys(classSCT)
-sctclass.send_keys(Keys.TAB)
+total = re.search(regex0, searched)
+error = re.search(regex1, searched)
 
-# find submit button and click it to search cases
-browser.find_element_by_xpath(sbtpath).click()
+a = re.search(regex1, browser.find_element_by_xpath(error).text)
 
-# check if elements are visible in first page and wait if not
-visible = EC.presence_of_element_located((By.XPATH, results))
 
-# extract total number of case hits
+name = 'Nathan Jensen'
+regex0 = re.compile('(?<=de )[0-9]+', re.IGNORECASE)
+regex1 = re.compile('[0-9]+', re.IGNORECASE)
+
 total = browser.find_element_by_xpath(results).text
-total = re.search(regex0, total)[0]
 
-# total pages
-pages = math.ceil(int(total) / 10)
+re.search(regex1, 'Não foi encontrado nenhum resultado correspondente à busca realizada.') == None
 
-# find all case numbers in first page and get them
 casenumbers = browser.find_elements_by_xpath(numbers)
 casenumbers = [x.text for x in casenumbers[:10]]
 
-# run different loops if there are multiple pages
-if pages > 1:
-    # for loop to construct list of case numbers in other pages
-    for i in range(1, pages):
-        # click to advance pages
-        browser.find_element_by_xpath(nextpage).click()
-        time.sleep(1)
-        # get additional case numbers
-        extranumbers = browser.find_elements_by_xpath(numbers)
-        extranumbers = [x.text for x in extranumbers[:10]]
-        # extend case numbers list
-        casenumbers.extend(extranumbers)
+if re.search(regex1, casenumbers[0]) == None:
+    'There are no SCT cases for candidate ' + name + '.'
+
+browser.quit()
+exit()
--- tjsp.py
+++ tjsp.py
@@ -46,16 +46,6 @@ class scraper:
         # browser
         self.browser = browser
 
-    # case number scraper function
-    def case(self, name):
-        """method to download case numbers by candidate information"""
-        
-        # search parameters
-        # not available
-
-        # return call
-        return 'This method has not been developed yet'
-
     # sct case number scraper function (special civil tribunals)
     def sct_case(self, name):
         """method to narrow in on sct cases from list of cases"""
@@ -75,14 +65,15 @@ class scraper:
         # click to search 
         sbtpath = '//*[(@id = "pbSubmit")]'
 
+        # find search results
+        search = '//*[contains(@class, "espacamentoCimaBaixo")]'
+
         # find total number of cases in page
-        numbers = '//*[contains(concat( " ", @class, " " ),' + \
-            ' concat( " ", "fonteNegrito", " " ))]'
+        numbers = '//*[contains(@class, "fonteNegrito")]'
 
         # fint total number of cases in all pages and extract it
-        regex0  = re.compile('(?<=de )[0-9]+', re.IGNORECASE)
-        results = '//*[@class = "espacamentoCimaBaixo"]' + \
-            '//*[contains(text(), "Resultados")]'
+        regex0 = re.compile('(?<=de )[0-9]+', re.IGNORECASE)
+        regex1 = re.compile('[0-9]+', re.IGNORECASE)
         
         # find next page button
         nextpage = '//*[@title = "Próxima página"]'
@@ -100,47 +91,71 @@ class scraper:
             classbox.send_keys(Keys.TAB)
 
             # find submit button and click it to search cases
-            time.sleep(.5)
+            time.sleep(1)
             self.browser.find_element_by_xpath(sbtpath).click()
 
             # check if elements are visible in first page and wait if not
             time.sleep(2)
-            # visible = EC.presence_of_element_located((By.XPATH, results))
+
+            # check for error
+            searched = self.browser.find_element_by_xpath(search).text
 
             # extract total number of case hits
-            total = self.browser.find_element_by_xpath(results).text
-            total = re.search(regex0, total)[0]
+            total = re.search(regex0, searched)
 
+            # exit if no cases are found
+            if total == None: return 'No cases found for ' + self.name + '.'       
+            
             # total pages
+            total = re.search(regex0, searched)[0]
             pages = math.ceil(int(total) / 10)
 
             # find all case numbers in first page and get them
             casenumbers = self.browser.find_elements_by_xpath(numbers)
             casenumbers = [x.text for x in casenumbers[:10]]
-
+            
+            # continue if there are matches
             # run different loops if there are multiple pages
             if pages > 1:
                 # for loop to construct list of case numbers in other pages
-                for i in range(1, pages + 1):
-                    # click to advance pages
-                    self.browser.find_element_by_xpath(nextpage).click()
+                for i in range(pages - 1):
+                    if not i == pages - 1:
+                        # click to advance pages
+                        self.browser.find_element_by_xpath(nextpage).click()
                     time.sleep(1)
                     # get additional case numbers
                     extranumbers = self.browser.find_elements_by_xpath(numbers)
                     extranumbers = [x.text for x in extranumbers[:10]]
                     # extend case numbers list
                     casenumbers.extend(extranumbers)
+                    # break out in last iteration
+                    
 
             # wrangle data
             casenumbers = pd.DataFrame(casenumbers)
             casenumbers.columns = ['sctnumber']
 
+            # return outcome
+            return casenumbers
+
         # handle error
         except:
-            return 'There are no SCT cases for candidate ' + self.name + '.'
+            return 'There were errors when finding SCT cases for candidate ' \
+                + self.name + '.'
+
+    # case number scraper function
+    def case(self, name):
+        """method to download any case number by candidate information"""
+        
+        # search parameters
+        # not available
+
+        # return call
+        return 'This method has not been developed yet'
 
     # sct case decisions scraper function
     def decision(self, number):
+        """method to download case decisions by candidate information"""
         
         # search parameters
         # not available
